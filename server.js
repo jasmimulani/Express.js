9bdbec0bd291eea2dd38173a44fa39e673a7edc7
Jasmin <jasmimulani@gmail.com>
2024-08-07 20:56:17 +0530

    2 type application or router

--- server.js
+++ server.js
@@ -3,12 +3,24 @@ const  express = require('express')
 //   create server in express
  const server = express();
 
-//   import json file
-const fs = require ('fs');
-const data = fs.readFileSync('./user.json','utf-8');
-// console.log(data);
+// //   import json file
+// const fs = require ('fs');
+// const data = fs.readFileSync('./user.json','utf-8');
+// // console.log(data);
 
 
+//  middelware function
+const myfun =(req,res,next) =>{
+  // console.log(res.query);
+  if(req.query.age >=18){
+    console.log(('sucesss......'));
+    next();
+  }else{
+    res.json({msg:"sorry try agin......."})
+  }
+  
+}
+// server.use(myfun)  //application
 
 //   post,   -> create 
 //  get, => redrive or get
@@ -21,33 +33,39 @@ server.get('/',(req, res) =>{
     res.end()
   });
 
+  server.get('/login' ,myfun ,(req,res)=>{
+    res.write('welcom login page')
+    res.end()
+    })
+
+    server.post("/" ,(req,res) =>{
+      res.send('<h1> this is post method </h1>')
+    })
+
   // server.get('/',(req, res) =>{   //-----------> same end point and same get method so that time alway 1st code is executed..........
   //   res.send('welcome to express');
   //   res.end()
   // });
 
-  server.post('/',(req,res) =>{
-    // res.write('use write method server end complesory')
-    res.send('<h1> post method not end server to complsory</h1>')
-  })
+  // server.post('/',(req,res) =>{
+  //   // res.write('use write method server end complesory')
+  //   res.send('<h1> post method not end server to complsory</h1>')
+  // })
 
-  server.put("/",(req,res) =>{
-    res.json({msg:"hello this os json formate data"})
-  })
+  // server.put("/",(req,res) =>{
+  //   res.json({msg:"hello this os json formate data"})
+  // })
 
-    server.patch('/' ,(req,res) =>{
-      res.status(250);
-      res.json({msg:"hello this os json formate data"})
-    })
+  //   server.patch('/' ,(req,res) =>{
+  //     res.status(250);
+  //     res.json({msg:"hello this os json formate data"})
+  //   })
 
-    server.get('/user',(req,res) =>{
-         res.json(JSON.parse(data));
-    })
+  //   server.get('/user',(req,res) =>{
+  //        res.json(JSON.parse(data));
+  //   })
 
-    server.get('/login' ,(req,res)=>{
-    res.write('welcom login page')
-    res.end()
-    })
+  
 
    server.listen(8000,() =>{
     console.log('server start at  http://localhost:8000');
