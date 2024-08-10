9e9d79d32854b41b17ddbef34e779133120abeed
Jasmin <jasmimulani@gmail.com>
2024-08-10 18:43:37 +0530

    "api-2"

--- app.js
+++ app.js
@@ -34,6 +34,34 @@ app.get("/user",(req,res) =>{
         let item = users.find((User) =>User.id === id)
         res.json(item);
 });
+
+//  replace data - put
+
+app.put("/user/:id",(req,res) =>{
+    let id = +req.params.id;
+    let userindex = users.findIndex((item) => item.id === id);
+    users.splice(userindex , 1 , req.body);
+    res.json({msg:'user replaced succesfully'});
+})
+
+//  update  data -patch
+
+ app.patch("/user/:id",(req,res) =>{
+    let id = +req.params.id;
+    let userindex = users.findIndex((item) => item.id === id);
+     let user = users[userindex];
+     users.splice(userindex , 1, {...user, ...req.body});
+     res.json({msg:" user update succesfully"})
+ })
+
+
+ app.delete("/user/:id",(req,res) =>{
+    let id = +req.params.id;
+    let userindex = users.findIndex((item) => item.id ===id);
+    users.splice(userindex,1);
+    res.json({msg :"user delete sucsessss"});
+ });
+
 app.listen(3030,() =>{
     console.log(`server start at http://localhost:3030`);
 })
