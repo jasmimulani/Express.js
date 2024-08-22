eaae1adcbe2e1f3e51c7e7657a7f2e9fc17b942d
Jasmin <jasmimulani@gmail.com>
2024-08-22 19:35:35 +0530

    token genret and sign-in or sign-up

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,5 +1,7 @@
   const User = require("../model/user.model")
    const bcrypt = require("bcrypt");
+   const jwt = require('jsonwebtoken')
+
 
    exports.registerUser = async (req,res)=>{
     try{
@@ -11,7 +13,7 @@
         // console.log(hashPassword);
         user =  await User.create({...req.body, password:hashPassword});
 
-        res.status(201).json({user, msg:"register sucsses..."});
+        res.status(201).json({user, msg:"register sucsses..."}); 
     }catch(err){
         console.log(err);
         res.status(500).json({msg:"internal server error"});
@@ -31,9 +33,36 @@
             if(!comparedpassword){
                 return res.json({msg:"incorrect pass or email..."})
             }
-            res.status(200).json({msg:"login success",user});
+
+            let token = await jwt.sign({userId:user._id},process.env.JWT_SECRET);
+            console.log(token);  //token console log 
+            
+            
+            res.status(200).json({msg:"login success..", user});
         }catch (err){
             console.log(err);
             res.status(500).json({msg:"internal server error"});
         }
-      }
+      };
+
+        exports.getprofile =  async(req,res)=>{
+            try{
+                res.json(req.user);
+            }catch(err){
+                console.log(err);
+                res.status(500).json({msg:"server error"})
+                
+            }
+        };
+
+        exports.updateprofile = async (req,res)=>{
+            try {
+                  let user = req.user;
+                  user = await User.findByIdAndUpdate(user._id,{$set:req.body},{new:true});
+                  res.status(202).json({user, msg:"user profile updated..."})
+            } catch (err) {
+                console.log(err);
+                res.status(500).json({ msg:"internal server error"})
+                
+            }
+          }
--- routers/user.routes.js
+++ routers/user.routes.js
@@ -2,9 +2,12 @@ const express = require("express");
 
 const userRotes = express.Router();
 
- const { registerUser,  loginUser } = require("../controller/user.controller")
+ const { registerUser,  loginUser, getprofile, updateprofile } = require("../controller/user.controller");
+const { verifyToken } = require("../helpers/verifyToken");
 
-userRotes.post('/signup' , registerUser)
-userRotes.post('/signin' , loginUser)
+userRotes.post('/signup' , registerUser);
+userRotes.post('/signin' , loginUser);
+userRotes.get('/me' , verifyToken,getprofile);
+userRotes.put('/update' , verifyToken, updateprofile);
 
 module.exports = userRotes;
